8120f9c975963c815f3eba16f9b9c7d6b379979f
merabi <merabi.vasadze.1@iliauni.edu.ge>
2020-08-27 22:16:18 +0400

    Error fixed

--- FlaskApp.py
+++ FlaskApp.py
@@ -28,5 +28,11 @@ api.add_resource(AddNewUser, '/AddNewUser/<string:name>')
 if __name__ == "__main__":
     from db import db
     db.init_app(app)
+
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
     print("Demo line")
     app.run(port=5000, debug=True)
