0bbf51c3352333e29225ba44a7b66da2387c5121
merabi <merabi.vasadze.1@iliauni.edu.ge>
2020-09-11 11:36:45 +0400

    added bonus/penalty directory

---
+++ modules/bonus_penalty/bonus.py
@@ -0,0 +1,18 @@
+def calculate_bonus(sale_amount, percentage):
+    pass
+
+
+def bonus_by_sales():
+    pass
+
+
+def bonus_by_amount():
+    pass
+
+
+def bonus_by_job_efficiency():
+    pass
+
+
+
+
